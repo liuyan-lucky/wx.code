fbfaa103d0b4d7e18fceeca791d2dcf37bb9ec3d
刘燕 <45897078+liuyan-lucky@users.noreply.github.com>
2019-01-10 13:04:42 +0800

    Add files via upload

---
+++ wxproject/pages/home/home.js
@@ -0,0 +1,73 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+// console.log("页面加载")
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    // console.log("页面初次渲染完成")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // console.log("监听页面显示")
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    // console.log("页面隐藏")
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    // console.log("页面卸载")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.setData({
+      name:"hhee"
+    })
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+return{
+  title:"your life",
+  path:"/pages/my/my"
+}
+  }
+})
